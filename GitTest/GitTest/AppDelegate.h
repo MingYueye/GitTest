b806e86c139324b11bc3de8306325149ee6a19f2
MingYueye <290630027@qq.com>
2016-06-12 09:41:27 +0800

    原版

---
+++ GitTest/GitTest/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  GitTest
+//
+//  Created by lanou3g on 16/6/12.
+//  Copyright © 2016年 lol. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
